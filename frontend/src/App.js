9d30ae879d23863ccbefb38bc654fdf9ef9324e0
oliviafoster <oliviarfoster@gmail.com>
2021-02-17 15:18:22 -0800

    Adding bookings calendar to main page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,16 +4,20 @@ import Dashboard from './screens/Dashboard';
 import Reservation from './screens/Reservation';
 import Mail from './screens/Mail';
 import Social from './screens/Social';
+import BookingsCalendar from "./components/reservation/BookingsCalendar";
 import './App.css';
 
 function App() {
     return (
-        <Router>
-            <Route exact path="/" component={Dashboard} />
-            <Route exact path="/reservation" component={Reservation} />
-            <Route exact path="/mail" component={Mail} />
-            <Route exact path="/social" component={Social} />
-        </Router>
+        <div>
+            <Router>
+                <Route exact path="/" component={Dashboard} />
+                <Route exact path="/reservation" component={Reservation} />
+                <Route exact path="/mail" component={Mail} />
+                <Route exact path="/social" component={Social} />
+            </Router>
+            <BookingsCalendar/>
+        </div>
     );
 }
 
