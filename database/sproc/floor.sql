ab9b6483ab03ab5c43f66177545386ad975d1123
Ming <mingjiang@live.nl>
2021-02-17 16:58:06 -0800

    added procedures to db files

--- database/sproc/floor.sql
+++ database/sproc/floor.sql
@@ -4,3 +4,11 @@ BEGIN
 SELECT * FROM `floor` where `fk_office_id`=`office_id` and `fk_office_location`=`office_location`;
 
 END
+
+CREATE PROCEDURE `createFloor` (IN `floor_num` INT(8), IN `office_id` INT(8), IN `office_location` VARCHAR(50), IN `floor_plan` IMAGE)
+BEGIN
+
+INSERT INTO `floor` (`floor_num`, `fk_office_id`, `fk_office_location`, `floor_plan`)
+	VALUES (`floor_num`, `office_id`, `office_location`, `floor_plan`);
+
+END
