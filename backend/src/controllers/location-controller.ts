f4eb824e8c0b394037639e9ab2ef4d2c9d3516ad
Ming <mingjiang@live.nl>
2021-02-17 17:04:26 -0800

    filled out skeleton of add-location

---
+++ backend/src/controllers/location-controller.ts
@@ -0,0 +1,15 @@
+import {Request, Response, NextFunction} from 'express';
+
+export default class LocationController {
+    // tslint:disable-next-line:no-empty
+    constructor() {}
+
+    /*
+        Adds an office into the database.
+        @param: req is the request, which contains the office in req.body
+        @return: promise of type boolean to signify if adding location was successful or not.
+    */
+    addLocation(req: Request): Promise<boolean> {
+        const office: Office = JSON.parse(req.body);
+    }
+}
