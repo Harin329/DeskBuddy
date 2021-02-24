997091b013ded3a7a839b913ca53c0c3267aa3fc
Ming <mingjiang@live.nl>
2021-02-23 16:36:15 -0800

    add location form almost complete

--- frontend/src/components/reservation/AddLocationForm.jsx
+++ frontend/src/components/reservation/AddLocationForm.jsx
@@ -11,6 +11,7 @@ const styles = theme => ({
         padding: '0 30px',
         marginTop: '10px',
         marginBottom: '10px',
+        marginRight: '10px',
         fontFamily: 'Lato',
         fontWeight: 'bolder',
         fontSize: 18
@@ -39,11 +40,12 @@ const styles = theme => ({
     addLocation: {
         position: 'fixed',
         top: '20%',
-        left: '30%',
-        width: '40%',
-        height: 'auto',
+        left: '25%',
+        width: '45%',
+        height: '60%',
         background: '#FFFCF7',
         padding: '30px',
+        overflow: 'auto'
     }
 });
 
@@ -57,11 +59,93 @@ class AddLocationForm extends React.Component {
         }
     }
 
+    getLowestID() {
+        const floor_ids = [];
+        for (let floor of this.state.inputFloors) {
+            floor_ids.push(floor.floor_id);
+        }
+        for (let i = 1; i < 500; i++) {
+            if (!floor_ids.includes(i)) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
     addFloor() {
-        const floor = []
+        const floor = { floor_id: this.getLowestID.bind(this), floor_num: 0, floor_desks: "" }
         this.setState(prevState => ({
             inputFloors: [...prevState.inputFloors, floor]
         }));
+    }
+
+    deleteFloor(id) {
+        this.setState(prevState => ({
+            inputFloors: prevState.inputFloors.filter((floor) => {
+                return floor.floor_id != id;
+            })
+        }));
+    }
+
+    handleSubmit(id) {
+
+    }
+
+    handleDeskInput(id) {
+        this.setState(prevState => ({
+            inputFloors: prevState.inputFloors.map((floor) => {
+                if (floor.floor_id === id) {
+                    
+                }
+                return floor;
+            })
+        }));
+    }
+
+
+    renderFloors() {
+        const { classes } = this.props;
+
+        let content = this.state.inputFloors.map((floor) =>
+            <div>
+                <div>
+                    <TextField
+                        id="num_id"
+                        label="Floor Number"
+                        style={{ margin: 8 }}
+                        placeholder="1234 Test Road"
+                        variant="outlined"
+                        margin="normal"
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                    />
+                    <Button className={classes.actionButton}>
+                        Attach Image
+                    </Button>
+                    <Button className={classes.actionButton} onClick={this.deleteFloor.bind(this, floor.floor_id)}>
+                        Remove Floor
+                    </Button>
+                </div>
+                <div>
+                    <TextField
+                        id="desks_id"
+                        label="Semicolon-separated desk ID's with capacities"
+                        style={{ margin: 8 }}
+                        placeholder="01-1;02-4;03-11"
+                        variant="outlined"
+                        fullWidth
+                        margin="normal"
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                        onChange={this.handleDeskInput.bind(this)}
+                    /></div>
+            </div>);
+
+        return (<div>
+            {content}
+        </div>)
     }
 
     render() {
@@ -97,10 +181,11 @@ class AddLocationForm extends React.Component {
                         }}
                     /></div>
                     <div>
-                        <Button className={classes.actionButton} onClick={this.addFloor}>
+                        <Button className={classes.actionButton} onClick={this.addFloor.bind(this)}>
                             Add Floor
                         </Button>
                     </div>
+                    {this.renderFloors.bind(this)()}
                     <div>
                         <Button className={classes.actionButtonCenter}>
                             Publish
