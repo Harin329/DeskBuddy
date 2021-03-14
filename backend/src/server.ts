efbe2833f0aed17d910c3daa5dc5a51de9fde42b
Harin <master.harin@gmail.com>
2021-03-13 17:52:23 -0800

    auth deployment working

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,4 @@
 import express, { Request, Response } from 'express';
-import https from 'https';
-import fs from 'fs';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import reservationRoute from './routes/reservation-routes';
@@ -17,22 +15,16 @@ import DB from './config/db-handler';
 
 export class DeskbuddyServer {
   private app: any;
-  private server: any;
   private port: number;
   private handler: any;
 
   constructor(port: number) {
-    // const key = fs.readFileSync('key.pem'); // Dev
-    // const cert = fs.readFileSync('cert.pem'); // Dev
-
     this.port = port;
     this.app = express();
     this.app.use(cors());
     this.app.use(bodyParser.json());
     this.app.use(bodyParser.urlencoded({ extended: true }));
     this.app.use(authRoute);
-
-    // this.server = https.createServer({key: key, cert: cert}, this.app); // Dev
 
     this.app.get('/', (req: Request, res: Response) => {
       res.send('Hello DeskBuddy!');
--- frontend/src/config/Constants.jsx
+++ frontend/src/config/Constants.jsx
@@ -1,6 +1,6 @@
 // Dev
-const Endpoint = 'https://localhost:3000'
-export const EndpointFE = 'https://localhost:3001'
+const Endpoint = 'http://localhost:3000'
+export const EndpointFE = 'http://localhost:3001'
 
 // Prod
 // export const EndpointFE = 'https://d2y5vcfqq9yc1u.cloudfront.net'
