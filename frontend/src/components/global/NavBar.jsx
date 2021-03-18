70725d28e0033d8c28b500317923699154ad23ef
Harin <master.harin@gmail.com>
2021-03-18 15:30:58 -0700

    tabbar + mod1 cleanup

--- frontend/src/components/global/NavBar.jsx
+++ frontend/src/components/global/NavBar.jsx
@@ -1,10 +1,26 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import ICBC from "../../assets/ICBC.png";
+import { Link, NavLink } from 'react-router-dom';
 import { useMsal } from "@azure/msal-react";
+import { makeStyles } from '@material-ui/core/styles';
 import { apiConfig, graphConfig } from "../../authConfig";
 import safeFetch, { graphFetch, accountIsAdmin } from "../../util/Util";
 
+const useStyles = makeStyles({
+    linkText: { 
+        color: 'gray', 
+        height: '30%', 
+        alignSelf: 'center', 
+        flex: 4, 
+        textAlign: 'center', 
+        textDecoration: 'none',
+        fontFamily: 'Lato',
+        fontWeight: 'bold'
+    },
+});
+
 function NavBar() {
+    const classes = useStyles();
 
     const { instance, accounts } = useMsal();
     const userOID = accounts[0].idTokenClaims.oid;
@@ -36,37 +52,37 @@ function NavBar() {
     }
 
     return (
-        <div style={{background:'black'}}>
-
-            <div style={{color: 'white', display: "flex"}} >
-
-                <Link to="/"
-                      className="App-link"
-                >
-                    Dashboard
-                </Link>&nbsp;&nbsp;&nbsp;
-                <Link to="/reservation"
-                      className="App-link"
-                >
-                    Reserve Desk
-                </Link>&nbsp;&nbsp;&nbsp;
-                <Link to="/mail"
-                      className="App-link"
-                >
-                    Mail Room
-                </Link>&nbsp;&nbsp;&nbsp;
-                <Link to="/social"
-                      className="App-link"
-                >
-                    Social
-                </Link>&nbsp;&nbsp;&nbsp;
-                <button style={{ height: '25px' }} onClick={() => callDeskBuddyEndpoint()} > API CALL </button>&nbsp;&nbsp;&nbsp;
-                <button style={{ height: '25px' }} onClick={() => callGraphEndpoint()} > MS GRAPH CALL </button>&nbsp;&nbsp;&nbsp;
-                <button style={{ height: '25px' }} onClick={() => instance.logout()} > LOGOUT </button>
+        <div style={{ background: 'white', flexDirection: 'row', display: 'flex', padding: 20 }}>
+            <div style={{ display: "flex", flex: 1 }} >
+                <div style={{ flex: 1 }} />
+                <img src={ICBC} style={{ width: 50, height: 50, marginRight: 30, flex: 1 }} />
+                <NavLink to="/" exact={true} className={classes.linkText} activeStyle={{color: 'black'}}>
+                    DASHBOARD
+                </NavLink>
+                <NavLink to="/reservation" className={classes.linkText} activeStyle={{color: 'black'}}>
+                    RESERVE DESK
+                </NavLink>
+                <NavLink to="/mail" className={classes.linkText} activeStyle={{color: 'black'}}>
+                    MAIL ROOM
+                </NavLink>
+                <NavLink to="/social" className={classes.linkText} activeStyle={{color: 'black'}}>
+                    SOCIAL FEED
+                </NavLink>
+                <div style={{ flex: 15, alignSelf: 'center', justifyContent: 'center' }} >
+                    {/* <button style={{ height: '25px' }} onClick={() => callDeskBuddyEndpoint()} > API CALL </button>
+                    <button style={{ height: '25px' }} onClick={() => callGraphEndpoint()} > MS GRAPH CALL </button> */}
+                </div>
+                <div style={{ color: 'black', alignSelf: 'center', flex: 1, fontFamily: 'Lato' }}>
+                    {username} {"\n"}
+                    {userOID} {"\n"}
+                    {isAdmin ? "Admin" : "User"}
+                </div>
+                <div style={{ flex: 1 }} />
+                <img src={ICBC} style={{ width: 50, height: 50, borderRadius: 50, backgroundColor: 'black', flex: 1 }} />
+                <div style={{ flex: 1 }} />
+                <button style={{ height: '25px', alignSelf: 'center', fontFamily: 'Lato' }} onClick={() => instance.logout()} > LOGOUT </button>
+                <div style={{ flex: 1 }} />
             </div>
-            <div style={{color: 'white'}}> username: {username} </div>
-            <div style={{color: 'white'}}> oid: {userOID} </div>
-            <div style={{color: 'white'}}> isAdmin: {isAdmin.toString()} </div>
 
         </div>
     );
--- frontend/src/screens/Dashboard.jsx
+++ frontend/src/screens/Dashboard.jsx
@@ -1,27 +1,28 @@
 import '../App.css';
-import { Button, List, ListItem, Grid, Typography, Divider, Modal, IconButton } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 import Title from '../components/global/Title';
 import Subheader from '../components/reservation/Subheader';
 import UpcomingReservations from '../components/reservation/UpcomingReservations';
+import { isMobile } from 'react-device-detect';
 
 function Dashboard() {
   return (
     <div className="App">
       {Title('DASHBOARD', 1, 10, 1)}
-      <Grid container direction='row' justify='center' alignItems='center'>
+      <Grid container direction={isMobile ? 'column' : 'row'} justify='center' alignItems='center'>
         <Grid item xs={1} />
-        <Grid item xs={4} style={{ backgroundColor: '#353B3C', height: '70vh' }} >
+        <Grid item xs={isMobile ? 10 : 4} style={{ backgroundColor: '#353B3C', height: '70vh', padding: 20 }} >
           {Subheader('TODAY (TODO)', 3, 6, 3)}
           {Subheader('UPCOMING RESERVATIONS', 2, 8, 2)}
           {UpcomingReservations()}
         </Grid>
         <Grid item xs={1} />
-        <Grid container item direction='row' xs={5}  style={{ height: '70vh' }}>
-          <Grid item xs={12} style={{ backgroundColor: '#353B3C' }}>
+        <Grid container item direction='row' xs={isMobile ? 10 : 5}  style={{ height: '70vh' }}>
+          <Grid item xs={12} style={{ backgroundColor: '#353B3C', padding: 20 }}>
             {Subheader('COVID-19 UPDATES (TODO)', 3, 6, 3)}
           </Grid>
           <Grid item xs={1} />
-          <Grid item xs={12} style={{ backgroundColor: '#353B3C' }}>
+          <Grid item xs={12} style={{ backgroundColor: '#353B3C', padding: 20 }}>
             {Subheader('MY MAIL (TODO)', 3, 6, 3)}
           </Grid>
         </Grid>
