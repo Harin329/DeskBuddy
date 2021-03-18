18af58f4bf7a969a7f23454bbe3d9880692a52cb
Ming <mingjiang@live.nl>
2021-03-18 16:17:41 -0700

    added POST /mail

--- backend/src/routes/mail-routes.ts
+++ backend/src/routes/mail-routes.ts
@@ -1,19 +1,42 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response, response } from 'express';
 
 const router = Router();
 
 import MailController from '../controllers/mail-controller';
 const mailServer = new MailController();
 
+router.post('/', (req: Request, res: Response) => {
+    const body = req.body;
+    if (body === undefined || body === {}) {
+        res.status(400).json({
+            err: "Malformed request body"
+        })
+    } else {
+        mailServer.createMail(req.body).then((value: number) => {
+            res.status(200).json({
+                id: value.toString()
+            });
+        }).catch((err: any) => {
+            res.status(404).json({
+                err,
+            });
+        })
+    }
+});
 
-// router.get('/getFeedByCategory/:category', (req: Request, res: Response) => {
-//     postServer.findPostByCategory(Number(req.params.category))
-//     .then((posts: any) => {
-//         res.json(posts);
-//     })
-//     .catch((err: any) => {
-//         res.json(err);
-//     })
-// })
+router.delete('/:id', (req: Request, res: Response) => {
+    const id = parseInt(req.params.id, 10);
+    if (!id) {
+        res.status(400).json({
+            err: "Malformed request body"
+        });
+    } else {
+        mailServer.deleteMail(id).then((affectedRows: number) => {
+            res.status(200).json({
+                affectedRows,
+            });
+        })
+    }
+});
 
 export default router;
