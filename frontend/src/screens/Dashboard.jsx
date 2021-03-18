f6ca141f7d4cff85877319b24065fa7d69c35a24
Harin <master.harin@gmail.com>
2021-03-18 12:30:34 -0700

    calendar connection

--- frontend/src/screens/Dashboard.jsx
+++ frontend/src/screens/Dashboard.jsx
@@ -1,33 +1,32 @@
 import '../App.css';
-import logo from '../logo.svg';
+import { Button, List, ListItem, Grid, Typography, Divider, Modal, IconButton } from '@material-ui/core';
+import Title from '../components/global/Title';
+import Subheader from '../components/reservation/Subheader';
+import UpcomingReservations from '../components/reservation/UpcomingReservations';
 
 function Dashboard() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to DeskBuddy
-        </p>
-        <a
-          className="App-link"
-          href="/reservation"
-        >
-          Reserve Desk
-        </a>
-        <a
-          className="App-link"
-          href="/mail"
-        >
-          Mail Room
-        </a>
-        <a
-          className="App-link"
-          href="/social"
-        >
-          Social
-        </a>
-      </header>
+      {Title('DASHBOARD', 1, 10, 1)}
+      <Grid container direction='row' justify='center' alignItems='center'>
+        <Grid item xs={1} />
+        <Grid item xs={4} style={{ backgroundColor: '#353B3C', height: '70vh' }} >
+          {Subheader('TODAY (TODO)', 3, 6, 3)}
+          {Subheader('UPCOMING RESERVATIONS', 2, 8, 2)}
+          {UpcomingReservations()}
+        </Grid>
+        <Grid item xs={1} />
+        <Grid container item direction='row' xs={5}  style={{ height: '70vh' }}>
+          <Grid item xs={12} style={{ backgroundColor: '#353B3C' }}>
+            {Subheader('COVID-19 UPDATES (TODO)', 3, 6, 3)}
+          </Grid>
+          <Grid item xs={1} />
+          <Grid item xs={12} style={{ backgroundColor: '#353B3C' }}>
+            {Subheader('MY MAIL (TODO)', 3, 6, 3)}
+          </Grid>
+        </Grid>
+        <Grid item xs={1} />
+      </Grid>
     </div>
   );
 }
