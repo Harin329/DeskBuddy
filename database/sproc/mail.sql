c6f688006fb0ca8df5929d716b2864a9d3be48f2
billz <billzhou64@gmail.com>
2021-03-26 18:05:24 -0600

    finished update query procedure (will likely need to add another update procedure).

--- database/sproc/mail.sql
+++ database/sproc/mail.sql
@@ -1,36 +1,25 @@
 CREATE PROCEDURE `createMail` (IN `eID` VARCHAR(50), IN `aID` VARCHAR(50), IN `oID` INT(8), IN `oLoc` VARCHAR(50), IN `arrive` DATE, 
                                 IN `tp` VARCHAR(50), IN `dim` VARCHAR(50), IN `send` VARCHAR(50), IN `notes` VARCHAR(255))
-
 BEGIN
-
 INSERT INTO `mail` (`fk_employee_id`, `fk_admin_eid`, `fk_office_id`, `fk_office_location`, `date_arrived`, `type`,
                         `dimensions`, `sender_info`, `additional_notes`)
 VALUES (`eID`, `aID`, `oID`, `oLoc`, `arrive`, `tp`, `dim`, `send`, `notes`);
 SELECT LAST_INSERT_ID();
-
 END
 
 CREATE PROCEDURE `deleteMail` (IN `mailID` INT(8))
-
 BEGIN
-
 DELETE FROM `mail` WHERE `mail_id` = `mailID`;
-
 END
 
 CREATE PROCEDURE `getMail` (IN `employeeID` VARCHAR(50))
-
 BEGIN
-
 SELECT * FROM `mail` WHERE `fk_employee_id` = `employeeID`;
-
 END
 
 CREATE PROCEDURE `createRequest` (IN `mailID` INT, IN `employeeID` VARCHAR(50), IN `employeeName` VARCHAR(50), IN `employeeEmail` VARCHAR(50), IN `employeePhone` VARCHAR(50),
-    IN `requestType` VARCHAR(50), IN `forwardLocation` VARCHAR(50), IN `additionalInstructions` VARCHAR(500), IN `reqCompletionDate` DATE, IN `modified_at`)
-
+    IN `requestType` VARCHAR(50), IN `forwardLocation` VARCHAR(50), IN `additionalInstructions` VARCHAR(500), IN `reqCompletionDate` DATE, IN `modified_at` DATE)
 BEGIN
-
 INSERT INTO `mail_request` (`mail_id`, `employee_id`, `employee_name`, `employee_email`, `employee_phone`,
                     `request_type`, `forward_location`, `additional_instructions`, `req_completion_date`,
                     `completion_date`, `status`, `admin_eid`, 'response', 'modifiedAt')
@@ -38,4 +27,15 @@ VALUES (`mailID`, `employeeID`, `employeeName`, `employeeEmail`, `employeePhone`
                             `additionalInstructions`, `reqCompletionDate`, null, null, null, null, `modified_at`);
 END
 
-CREATE PROCEDURE `updateRequest` (IN `mailID` INT, IN `employeeID` VARCHAR(50), IN ``)
+CREATE PROCEDURE `updateRequestEmployee` (IN `mailID` INT, IN `employeeID` VARCHAR(50), IN `employeePhone` VARCHAR(50),
+IN `requestType` VARCHAR(50), IN `forwardLocation` VARCHAR(50), IN `additionalInstructions` VARCHAR(500), IN `reqCompletionDate` DATE, in `modified_at` DATE)
+
+BEGIN
+
+UPDATE `mail_request`
+
+SET `employee_phone` = employeePhone,`request_type` = `requestType`, `forward_location` = `forwardLocation`, `additional_instructions` = `additionalInstructions`,
+    `req_completion_date` = `reqCompletionDate`, `modifiedAt` = `modified_at`
+
+WHERE `mail_id` = `mailID` && `employee_id` = `employeeID`;
+END
