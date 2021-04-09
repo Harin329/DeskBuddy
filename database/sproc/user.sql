7a055325e9e300127bd3590b8a1032dd3dd34259
Mitchell Currie <25001273+mitchsc@users.noreply.github.com>
2021-04-09 00:51:55 -0700

    sproc

--- database/sproc/user.sql
+++ database/sproc/user.sql
@@ -1,8 +1,8 @@
 CREATE PROCEDURE `insertEmployee`(IN `employee_id` VARCHAR(50), `first_name` VARCHAR(50), `last_name` VARCHAR(50), `phone` VARCHAR(50), `email` VARCHAR(50))
 
 BEGIN
-	INSERT INTO `employee` (`employee_id`, `first_name`, `last_name`, `email`, `phone`) VALUES(`employee_id`, `first_name`, `last_name`, `email`, `phone`) ON DUPLICATE KEY UPDATE    
-	employee.first_name=`first_name`, employee.last_name=`last_name`, employee.phone=`phone`, employee.email=`email`;
+	INSERT INTO `employee` (`employee_id`, `first_name`, `last_name`, `email`) VALUES(`employee_id`, `first_name`, `last_name`, `email`) ON DUPLICATE KEY UPDATE
+	employee.first_name=`first_name`, employee.last_name=`last_name`, employee.email=`email`;
 END
 
 CREATE PROCEDURE `getUserOIDByNameAndEmail` (IN `first` VARCHAR(50), IN `last` VARCHAR(50), IN `em` VARCHAR(50))
@@ -23,4 +23,11 @@ BEGIN
     FROM `employee` 
     WHERE employee_id = `oID`;
 
+END
+
+CREATE PROCEDURE `updateProfilePhoto`(IN `employee_id` VARCHAR(50), `photo` MEDIUMBLOB)
+BEGIN
+    UPDATE `employee`
+    SET employee.profile_photo = `photo`
+    WHERE employee.employee_id = `employee_id`;
 END
