0ea6f8056b21c97b72ad76857ef661398528b5d5
billz <billzhou64@gmail.com>
2021-04-11 18:29:27 -0600

    fixed modified-at date issue. Database now stores full date.

--- database/sproc/request.sql
+++ database/sproc/request.sql
@@ -1,26 +1,26 @@
 CREATE PROCEDURE `createRequest` (IN `mailID` INT, IN `employeeID` VARCHAR(50), IN `employeeName` VARCHAR(50), IN `employeeEmail` VARCHAR(50), IN `employeePhone` VARCHAR(50),
-                                     IN `requestType` VARCHAR(50), IN `forwardLocation` VARCHAR(50), IN `additionalInstructions` VARCHAR(500), IN `reqCompletionDate` DATE, IN `in_status` VARCHAR(50), IN `modified_at` DATE)
+IN `requestType` VARCHAR(50), IN `forwardLocation` VARCHAR(50), IN `additionalInstructions` VARCHAR(500), IN `reqCompletionDate` DATE, IN `in_status` VARCHAR(50))
 BEGIN
-INSERT INTO `mail_request` (`mail_id`, `employee_id`, `employee_name`, `employee_email`,
-                    `request_type`, `forward_location`, `additional_instructions`, `req_completion_date`,
-                    `completion_date`, `status`, `admin_eid`, `response`, `modified_at`)
-VALUES (`mailID`, `employeeID`, `employeeName`, `employeeEmail`, `requestType`, `forwardLocation`,
-                            `additionalInstructions`, `reqCompletionDate`, null, `in_status`, null, null, `modified_at`);
+    INSERT INTO `mail_request` (`mail_id`, `employee_id`, `employee_name`, `employee_email`, `employee_phone`,
+                                `request_type`, `forward_location`, `additional_instructions`, `req_completion_date`,
+                                `completion_date`, `status`, `admin_eid`, 'response', 'modified_at')
+    VALUES (`mailID`, `employeeID`, `employeeName`, `employeeEmail`, `employeePhone`, `requestType`, `forwardLocation`,
+            `additionalInstructions`, `reqCompletionDate`, null, `in_status`, null, null, now());
 END;
 
 CREATE PROCEDURE `updateRequestEmployee` (IN `mailID` INT, IN `employeeID` VARCHAR(50), IN `employeePhone` VARCHAR(50),
-IN `requestType` VARCHAR(50), IN `forwardLocation` VARCHAR(50), IN `additionalInstructions` VARCHAR(500), IN `reqCompletionDate` DATE, IN `in_status` VARCHAR(50), IN `modifiedAt` DATE)
+IN `requestType` VARCHAR(50), IN `forwardLocation` VARCHAR(50), IN `additionalInstructions` VARCHAR(500), IN `reqCompletionDate` DATE, IN `in_status` VARCHAR(50))
 BEGIN
 UPDATE `mail_request`
-SET `request_type` = `requestType`, `forward_location` = `forwardLocation`, `additional_instructions` = `additionalInstructions`,
-    `req_completion_date` = `reqCompletionDate`, `status` = `in_status`, `modified_at` = `modifiedAt`
+SET `employee_phone` = employeePhone,`request_type` = `requestType`, `forward_location` = `forwardLocation`, `additional_instructions` = `additionalInstructions`,
+    `req_completion_date` = `reqCompletionDate`, `status` = `in_status`, `modified_at` = now()
 WHERE `mail_id` = `mailID` AND `employee_id` = `employeeID`;
 END;
 
-CREATE PROCEDURE `updateRequestAdmin` (IN `mailID` INT, IN `in_status` VARCHAR(50), IN `adminEid` VARCHAR(50), IN `in_response` VARCHAR(50), IN `modifiedAt` DATE)
+CREATE PROCEDURE `updateRequestAdmin` (IN `mailID` INT, IN `in_status` VARCHAR(50), IN `adminEid` VARCHAR(50), IN `in_response` VARCHAR(50))
 BEGIN
 UPDATE `mail_request`
-SET `status` = `in_status`, `admin_eid` = `adminEid`, `response` = `in_response`, `modified_at` = `modifiedAt`
+SET `status` = `in_status`, `admin_eid` = `adminEid`, `response` = `in_response`, `modified_at` = now()
 WHERE `mail_id` = mailID;
 END;
 
